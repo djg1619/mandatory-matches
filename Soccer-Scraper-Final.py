e9ffee594e73576c6f51f49b4f8f5043705aa1fd
Dan Gomes <59592490+djg1619@users.noreply.github.com>
2020-10-19 13:47:41 -0400

    Add files via upload

---
+++ Soccer-Scraper-Final.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.goal.com/en-us/live-scores'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+match = soup.find('span',class_='match-row__date')
+
+competition = soup.find('span',class_='competition-name')
+
+home = soup.find('td', class_="match-row__team-home")
+
+away = soup.find('td', class_="match-row__team-away")
+
+for match,home,away in zip(soup.find_all('span', class_='match-row__date'),soup.find_all('td', class_="match-row__team-away"),soup.find_all('td', class_="match-row__team-home")):
+    print(match.text,home.text,'VS',away.text)
+
+
+# In[ ]:
+
+
+
+
